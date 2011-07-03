1faaf54502b80504568aa4f7d3110e6f9f8907d8
Richard Crowley <r@rcrowley.org>
2011-07-03 23:20:18 +0000

    Factored templates into config file.

--- pagerunit/__init__.py
+++ pagerunit/__init__.py
@@ -17,6 +17,18 @@ import types
 
 import mail
 
+# ConfigParser defaults are a bit limited so PagerUnit rolls its own.  These
+# are placed before the config files are opened.
+DEFAULTS = {'mail': {'problem_body': '{exc}\n\n\t{line}\n\n{doc}',
+                     'problem_subject': 'PROBLEM {name} on {fqdn}',
+                     'recovery_body': '{doc}',
+                     'recovery_subject': 'RECOVERY {name} on {fqdn}'},
+            'sms': {'problem': 'PROBLEM {name} on {fqdn}',
+                    'recovery': 'RECOVERY {name} on {fqdn}'},
+            'smtp': {'port': str(587),
+                     'server': 'smtp.gmail.com'},
+            'state': {'dirname': '/var/run/pagerunit'}}
+
 def _strip(s):
     """
     Strip whitespace from a multiline string.
@@ -49,17 +61,13 @@ class PagerUnit(object):
         Lazy-load the PagerUnit configuration file(s).
         """
         if not hasattr(self, '_cfg'):
-            self._cfg = ConfigParser(defaults={'dirname': '/var/run/pagerunit',
-                                               'port': str(587),
-                                               'server': 'smtp.gmail.com'})
+            self._cfg = ConfigParser()
+            for section, options in DEFAULTS.iteritems():
+                self._cfg.add_section(section)
+                for option, value in options.iteritems():
+                    self._cfg.set(section, option, value)
             self._cfg.read(['/etc/pagerunit.cfg',
                             os.path.expanduser('~/.pagerunit.cfg')])
-            for section, option in (('smtp', 'password'),
-                                    ('smtp', 'port'),
-                                    ('smtp', 'server'),
-                                    ('smtp', 'username'),
-                                    ('state', 'dirname')):
-                self._cfg.get(section, option)
         return self._cfg
 
     @property
@@ -82,12 +90,6 @@ class PagerUnit(object):
             self.run()
             time.sleep(secs)
 
-    _problem_subject = 'PROBLEM {name} on {fqdn}'
-    _problem_body = '''{exc}
-
-\t{line}
-
-{doc}'''
     def problem(self, f, exc, tb):
         """
         Add this problem to the runtime state and send a problem email.
@@ -100,28 +102,30 @@ class PagerUnit(object):
                          os.O_WRONLY | os.O_CREAT | os.O_EXCL,
                          0o644)
             os.close(fd)
-            logging.info('{0} has a problem'.format(f.__name__))
-            kwargs = dict(name=f.__name__,
-                          fqdn=socket.getfqdn(),
-                          exc=str(exc) or '(no explanation)',
-                          line=traceback.extract_tb(tb)[-1][-1],
-                          doc=_strip(f.__doc__))
-            if self.cfg.has_option('mail', 'address'):
-                self.mail.send_json(self.cfg.get('mail', 'address'),
-                                    self._problem_subject,
-                                    self._problem_body,
-                                    **kwargs)
-            if self.cfg.has_option('sms', 'address'):
-                self.mail.send(self.cfg.get('sms', 'address'),
-                               None,
-                               self._problem_subject,
-                               **kwargs)
         except OSError as e:
-            if errno.EEXIST != e.errno:
+            if errno.EEXIST == e.errno:
+                return
+            elif errno.ENOSPC == e.errno:
+                pass
+            else:
                 raise e
+        logging.info('{0} has a problem'.format(f.__name__))
+        kwargs = dict(name=f.__name__,
+                      fqdn=socket.getfqdn(),
+                      exc=str(exc) or '(no explanation)',
+                      line=traceback.extract_tb(tb)[-1][-1],
+                      doc=_strip(f.__doc__))
+        if self.cfg.has_option('mail', 'address'):
+            self.mail.send_json(self.cfg.get('mail', 'address'),
+                                self.cfg.get('mail', 'problem_subject'),
+                                self.cfg.get('mail', 'problem_body'),
+                                **kwargs)
+        if self.cfg.has_option('sms', 'address'):
+            self.mail.send(self.cfg.get('sms', 'address'),
+                           None,
+                           self.cfg.get('sms', 'problem'),
+                           **kwargs)
 
-    _recovery_subject = 'RECOVERY {name} on {fqdn}'
-    _recovery_body = '{doc}'
     def recovery(self, f):
         """
         Remove this now-recovered problem from the runtime state and send
@@ -130,23 +134,25 @@ class PagerUnit(object):
         try:
             os.unlink(os.path.join(self.cfg.get('state', 'dirname'),
                                    f.__name__))
-            logging.info('{0} recovered'.format(f.__name__))
-            kwargs = dict(name=f.__name__,
-                          fqdn=socket.getfqdn(),
-                          doc=_strip(f.__doc__))
-            if self.cfg.has_option('mail', 'address'):
-                self.mail.send_json(self.cfg.get('mail', 'address'),
-                                    self._recovery_subject,
-                                    self._recovery_body,
-                                    **kwargs)
-            if self.cfg.has_option('sms', 'address'):
-                self.mail.send(self.cfg.get('sms', 'address'),
-                               None,
-                               self._recovery_subject,
-                               **kwargs)
         except OSError as e:
-            if errno.ENOENT != e.errno:
+            if errno.ENOENT == e.errno:
+                return
+            else:
                 raise e
+        logging.info('{0} recovered'.format(f.__name__))
+        kwargs = dict(name=f.__name__,
+                      fqdn=socket.getfqdn(),
+                      doc=_strip(f.__doc__))
+        if self.cfg.has_option('mail', 'address'):
+            self.mail.send_json(self.cfg.get('mail', 'address'),
+                                self.cfg.get('mail', 'recovery_subject'),
+                                self.cfg.get('mail', 'recovery_body'),
+                                **kwargs)
+        if self.cfg.has_option('sms', 'address'):
+            self.mail.send(self.cfg.get('sms', 'address'),
+                           None,
+                           self.cfg.get('sms', 'recovery'),
+                           **kwargs)
 
     def run(self):
         """
